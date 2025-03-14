ceab14f03844cbb9a3da566e180644dcb9643654
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-14 10:45:20 +0300

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,23 @@
-const Home = () => {
+import SearchForm from "@/components/SearchForm";
+
+const Home = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) => {
+  const query = (await searchParams).query;
   return (
     <>
-      <h1 className="text-2xl heading"> Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your startup, <br /> connect with entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Ideas Noticed In Virtual
+          Competition.
+        </p>
+				<SearchForm query={query} />
+      </section>
     </>
   );
 };
