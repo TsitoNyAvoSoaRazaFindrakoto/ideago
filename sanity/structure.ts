8aec89363777377422d80b0fd47cdcdc73f7f84a
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-15 21:11:19 +0300

    Update structure.ts

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -2,6 +2,9 @@ import type {StructureResolver} from 'sanity/structure'
 
 // https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
-  S.list()
-    .title('Content')
-    .items(S.documentTypeListItems())
+	S.list()
+		.title('Content')
+		.items([
+			S.adocumentTypeListItem('author').title('Authors'),
+			S.adocumentTypeListItem('startup').title('Startupss'),
+		]);
