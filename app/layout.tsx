5b6b4687d6514e07d936dc71ddb0f0af4aa7c8f5
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-16 23:10:03 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
-import 'easymde/dist/easymde.min.css';
+import "./assets/globals.css";
+import "./assets/easymde/dist/easymde.min.css"
 
 const workSans = localFont({
 	src: [
-		{ path : './fonts/WorkSans-Black.ttf', weight : '900' , style : 'normal' },
-		{ path : './fonts/WorkSans-ExtraBold.ttf', weight : '800' , style : 'normal' },
-		{ path : './fonts/WorkSans-Bold.ttf', weight : '700' , style : 'normal' },
-		{ path : './fonts/WorkSans-SemiBold.ttf', weight : '600' , style : 'normal' },
-		{ path : './fonts/WorkSans-Medium.ttf', weight : '500' , style : 'normal' },
-		{ path : './fonts/WorkSans-Regular.ttf', weight : '400' , style : 'normal' },
-		{ path : './fonts/WorkSans-Thin.ttf', weight : '200' , style : 'normal' },
-		{ path : './fonts/WorkSans-ExtraLight.ttf', weight : '100' , style : 'normal' }
+		{ path : './assets/fonts/WorkSans-Black.ttf', weight : '900' , style : 'normal' },
+		{ path : './assets/fonts/WorkSans-ExtraBold.ttf', weight : '800' , style : 'normal' },
+		{ path : './assets/fonts/WorkSans-Bold.ttf', weight : '700' , style : 'normal' },
+		{ path : './assets/fonts/WorkSans-SemiBold.ttf', weight : '600' , style : 'normal' },
+		{ path : './assets/fonts/WorkSans-Medium.ttf', weight : '500' , style : 'normal' },
+		{ path : './assets/fonts/WorkSans-Regular.ttf', weight : '400' , style : 'normal' },
+		{ path : './assets/fonts/WorkSans-Thin.ttf', weight : '200' , style : 'normal' },
+		{ path : './assets/fonts/WorkSans-ExtraLight.ttf', weight : '100' , style : 'normal' }
 	],
 	variable : '--font-work-sans'
 })
