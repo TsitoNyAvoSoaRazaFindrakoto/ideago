65ba11805e2189871204ca228727732fa9efa17e
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-16 23:10:34 +0300

    Update env.ts

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,5 +1,5 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-15'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-16'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
