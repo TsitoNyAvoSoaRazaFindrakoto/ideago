61633b8229f85bbe8429d0d63851f88077160e93
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-17 14:38:05 +0300

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,7 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupCardType } from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
 import { startups_query } from "@/sanity/lib/queries";
-import StartupCardType from "@/types/post";
 
 const Home = async ({
   searchParams,
@@ -11,9 +10,7 @@ const Home = async ({
 }) => {
   const query = (await searchParams).query;
 
-	const posts = await client.fetch(startups_query);
-
-  console.log(JSON.stringify(posts, null, 2));
+  const posts = await client.fetch(startups_query);
   return (
     <>
       <section className="pink_container bg-primary">
@@ -31,10 +28,9 @@ const Home = async ({
         <p className="text-30-semibold">
           {query ? `Search results for "${query}"` : "All Startups"}
         </p>
-
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post: StartupCardType) => (
+            posts.map((post: any) => (
               <StartupCard key={post._id} post={post} />
             ))
           ) : (
