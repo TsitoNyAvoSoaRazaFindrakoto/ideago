fa9faf5c5ecf136112928e5998846fe7ecb4b838
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-17 14:38:01 +0300

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,13 @@
-import NavBar from '@/components/NavBar'
-import React from 'react'
+import NavBar from "@/components/NavBar";
+import React from "react";
 
-const Layout = ({ children } : Readonly<{ children : React.ReactNode}>) => {
-	return (
-		<main className='font-work-sans'>
-			<NavBar/>
-			{children}
-		</main>
-	)
-}
+const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <main className="font-work-sans">
+      <NavBar />
+      {children}
+    </main>
+  );
+};
 
-export default Layout
+export default Layout;
