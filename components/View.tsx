ecc58fa89651e24492987a43b08031f411b688ef
Razafindrakoto Tsito Ny Avo Soa <tsitonyavo@gmail.com>
2025-03-19 20:54:43 +0300

    Update View.tsx
    i look at you

--- components/View.tsx
+++ components/View.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Ping from "./Ping";
+import { client } from "@/sanity/lib/client";
+import { startup_views_query } from "@/sanity/lib/queries";
 
-const View = () => {
-	return (
-		<div>View</div>
-	)
-}
+const View = async ({ id }: { id: string }) => {
+  const { views : totalViews } = await client
+    .withConfig({ useCdn: false })
+		.fetch(startup_views_query, { id });
+	
+	// TODO : Update the number o views
+  return (
+    <div className="view-container">
+      <div className="absolute -top-2 -rigth-2">
+        <Ping />
+      </div>
 
-export default View
+      <p className="view-text">
+        <span className="font-black">Views: {totalViews} </span>
+      </p>
+    </div>
+  );
+};
+
+export default View;
