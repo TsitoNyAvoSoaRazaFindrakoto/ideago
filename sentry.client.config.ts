f8edeed5b7f104b82864b76c57039dbdb809c04c
Razafindrakoto Tsito Ny Avo Soa <153315298+TsitoNyAvoSoaRazaFindrakoto@users.noreply.github.com>
2025-04-09 10:43:49 +0300

    sentry config

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -3,9 +3,14 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://fae5016f5d6e58a176358c00057c1ef1@o4509119091703808.ingest.de.sentry.io/4509119172378704",
   integrations: [
+    Sentry.replayIntegration(),
     Sentry.feedbackIntegration({
       // Additional SDK configuration goes in here, for example:
       colorScheme: "system",
     }),
   ],
+  tracesSampleRate: 1,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  debug: false,
 });
